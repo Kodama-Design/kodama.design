aad06bbf7a3f0f5fa72f13dbd375513bc1060f01
Robert Parker <rparker@kodama.design>
2018-04-11 11:01:22 -0700

    fos user bundle

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
